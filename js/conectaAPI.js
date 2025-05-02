c41f0c9905606e1dd6e03ea4f154d81880cd1af3
mizae zuza <mizael.zuza.estudos@gmail.com>
2025-05-02 18:27:54 -0300

    primeira funcionalidade aplicada. Consumo da API e apresentação dos dados na página principal

---
+++ js/conectaAPI.js
@@ -0,0 +1,7 @@
+async function listarVideos() {
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+}
+
+export const conectaApi = {listarVideos};
